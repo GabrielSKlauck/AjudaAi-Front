e684a07a08d742dab0641ebe832786bad462004b
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2023-12-03 22:20:44 -0300

    inicio do javascript

--- addAnuncio.js
+++ addAnuncio.js
@@ -2,8 +2,16 @@ function showForm(){
     var formulario = document.getElementById("form-add");
 
     if (formulario.style.display === "none" || formulario.style.display === "") {
-        formulario.style.display = "flex";
+        formulario.style.display = "block";
     } else {
         formulario.style.display = "none";
     }
+}
+
+function criarAnuncio(){
+    var nomeOng = $("#input-nome-ong").val();
+    var descricaoOng = $("#ta-descricao").val();
+    console.log(nomeOng + " " + descricaoOng);
+
+    
 }
