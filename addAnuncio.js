b1fdd1b6a75da50407b3b1c5458a80ef3baaea8c
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2023-12-03 22:33:14 -0300

    Adiçao de anuncios feita
    Falta ajustes na aparência, porem esta funcional

--- addAnuncio.js
+++ addAnuncio.js
@@ -13,5 +13,10 @@ function criarAnuncio(){
     var descricaoOng = $("#ta-descricao").val();
     console.log(nomeOng + " " + descricaoOng);
 
-    
+    var novoAnuncio =  $("<div>").addClass("cards card border-2 p-2 flex flex-col items-center h-96");
+
+    novoAnuncio.append('<img src="assets/images/ong-exemplo.jpg" alt="" class="h-40 w-60">');
+    novoAnuncio.append('<h1 class="text-xl mt-4 mb-4">' + nomeOng +'</h1>');
+    novoAnuncio.append('<p class="text-center"> ' + descricaoOng + '</p>'); 
+    $(".container-interno").append(novoAnuncio);
 }
