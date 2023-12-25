36efd292d7f438f500c54bb24a3d53d5db56585a
Gabriel de Souza Klauck <112292289+GabrielSKlauck@users.noreply.github.com>
2023-12-25 19:19:56 -0300

    clique e pagina de anuncio feita
    ao clicar no anuncio vai para a pagina para se inscrever nele, falta fazer com abra a pagina de anuncio respectiva do anuncio clicado

--- addAnuncio.js
+++ addAnuncio.js
@@ -12,7 +12,7 @@ window.onload = function(){
 function mostraAds(item){
     item.forEach(linha => {
         const cardAds = `
-        <div class="container-interno grid grid-cols-1 mx-4 sm:mx-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3 ">
+        <div class="container-interno grid grid-cols-1 mx-4 sm:mx-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3" onclick="abrirPagina()">
         <div class="cards card border-2 p-2 flex flex-col items-center h-96">
           <img src="assets/images/logo anuncio.jpg" alt="" class="h-40 w-60">
           <h1 class="text-xl mt-4 mb-4">${linha.title}</h1>
@@ -23,4 +23,8 @@ function mostraAds(item){
        `;
         $(`#container`).append($(cardOng));
     });
+}
+
+function abrirPagina(){
+  window.location.href="paginaAnuncio.html"
 }
