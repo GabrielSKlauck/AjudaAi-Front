af8496bdbe5684dbfe77aff16d8877b753101235
Gabriel de Souza Klauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-01-05 19:55:54 -0300

    Carrega a pagina da ong respectiva

--- paginaOng.js
+++ paginaOng.js
@@ -2,6 +2,31 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
 
-window.onload = function(){
+var title = "";
+var desc = "";
+window.onload = function() {
+    request('GET', `ngo/${id}`, {}, function(result) {
+        title = result.ngoName;
+        desc = result.description;
+        console.log(title + " " + desc);
+        carregaPagina();
+    });
+};
 
+function carregaPagina(){       
+    var titulo = document.getElementById("title-ong");
+    var descricao = document.getElementById("description-ong");
+    titulo.innerHTML = title;
+    descricao.innerHTML = desc;
+}
+
+function request(method, url, headers, successCallback) {
+    $.ajax({
+        type: method,
+        url: `https://localhost:7070/${url}`,
+        headers: headers,
+        success: successCallback,
+        contentType: 'application/json',
+        dataType: 'json',
+    });
 }
