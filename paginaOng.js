f8ef6eee2fda74b22f6db3dfeef8c9e7d32332dd
Gabriel de Souza Klauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-01-05 21:29:13 -0300

    carrega ong feito, falta os seus respectivos anuncios

--- paginaOng.js
+++ paginaOng.js
@@ -8,17 +8,45 @@ window.onload = function() {
     request('GET', `ngo/${id}`, {}, function(result) {
         title = result.ngoName;
         desc = result.description;
-        console.log(title + " " + desc);
-        carregaPagina();
+       // console.log(title + " " + desc);
+        carregaOng();
+    });
+
+    $.ajax({
+        type: "GET",
+        url: `https://localhost:7070/ngo/NgoId/${id}`,
+        success: carregaAnuncios,
+        header: {},
+        contentType: "application/json",
+        datatype: "json",
     });
 };
 
-function carregaPagina(){       
+function carregaOng(){       
     var titulo = document.getElementById("title-ong");
     var descricao = document.getElementById("description-ong");
     titulo.innerHTML = title;
     descricao.innerHTML = desc;
 }
+
+function carregaAnuncios(){
+    console.log(item);
+    item.forEach(linha => {
+        
+        const cardAds = `
+        <div id = "${linha.id}" class="cards card flex flex-col items-center h-96" onclick="abrirPagina(${linha.id})">
+        <img src="assets/images/logo anuncio.jpg" alt="" class="h-40 w-60">
+        <h1 class="text-xl mt-4 mb-4">${linha.title}</h1>
+        <p class="text-center">
+            ${linha.description}
+        </p>
+      </div>
+        
+       `;
+        $(`.container`).append($(cardAds));
+    });
+}
+
 
 function request(method, url, headers, successCallback) {
     $.ajax({
