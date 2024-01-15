4ad47dc18105d8c9110fbb9d62ab458ed4e891c9
Jadina Maria Garcia <jadinagarcia@gmail.com>
2024-01-15 17:07:47 -0300

    Início do modal de foto de perfil do voluntário.

--- volunteerprofileedit.js
+++ volunteerprofileedit.js
@@ -3,3 +3,10 @@ let profileMenu = document.getElementById("profileMenu");
 function toggleProfileMenu(){
     profileMenu.classList.toggle("open-profile-menu");
 }
+
+let profileImg = document.getElementById("profile-img");
+let inputFile = document.getElementById("input-file");
+
+inputFile.onchange = function() {
+    profileImg.src = URL.createObjectURL(inputFile.files[0]);
+}
