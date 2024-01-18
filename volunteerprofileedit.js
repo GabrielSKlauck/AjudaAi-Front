38eed9b9072a9e4ad73c7c5f4c5f703718b9f8cf
Jadina Maria Garcia <jadinagarcia@gmail.com>
2024-01-18 20:17:41 -0300

    Finalização do HTML e CSS do modal de foto de perfil.

--- volunteerprofileedit.js
+++ volunteerprofileedit.js
@@ -1,12 +1,27 @@
 let profileMenu = document.getElementById("profileMenu");
 
-function toggleProfileMenu(){
+function toggleProfileMenu() {
     profileMenu.classList.toggle("open-profile-menu");
 }
 
 let profileImg = document.getElementById("profile-img");
 let inputFile = document.getElementById("input-file");
 
-inputFile.onchange = function() {
+inputFile.onchange = function () {
     profileImg.src = URL.createObjectURL(inputFile.files[0]);
+}
+
+function showModal() {
+    let modal = document.querySelector(".modal");
+    modal.classList.add(`modal-active`);
+}
+
+function closeModal() {
+    let modal = document.querySelector(".modal");
+    modal.classList.remove(`modal-active`);
+}
+
+let openModal = document.querySelectorAll(".open-modal");
+for (let open of openModal) {
+    open.addEventListener(`click`, showModal);
 }
