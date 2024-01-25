85b06ce2d1c48d044afeda2e0e19c83d31c03697
Gabriel de Souza Klauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-01-24 21:26:27 -0300

    Reforma do javascript feita

--- paginaAnuncio.js
+++ paginaAnuncio.js
@@ -5,35 +5,45 @@ const id = urlParams.get('id');
 
 var title = "";
 var desc = "";
+var expira = "";
+var nameOng = "";
+var ngoId = "";
 window.onload = function() {
     request('GET', `ads/${id}`, {}, function(result) {
         title = result.title;
         desc = result.description;
+        expira = result.expires;
+        ngoId = result.ngo_Id;
+    
+        console.log(ngoId);
+        request('GET', `ngo/${ngoId}`, {}, function(result) {
+        nameOng = result.ngoName;
         carregaPagina();
-    });
-
-    $.ajax({
-        type: "GET",
-        url: "https://localhost:7070/UserAds",
-        headers: {},
-        success: alteraBtnInsc,
-        contentType: "application/json",
-        dataType: "json",
+     });
     });
 };
 
-function carregaPagina(){       
+function carregaPagina(){   
         const pagina = `
-        <div id="div-pag" class="min-h-[333px] h-auto w-[43rem] mt-10 relative block rounded-xl">
-      <div class=" h-[235px] w-full flex">
-          <img id="img-ong" src="assets/images/ong-exemplo.jpg" alt="" class="h-[11rem] w-[11rem] ml-4 mt-8">
-          <h1 id="title-ngo" class="ml-5 mt-20 text-3xl">${title}</h1>
-      </div>
-      <p class="ml-1">
-                ${desc}
-                </p>
-                <button id="btn-subscribe" class="p-2 rounded-2xl float-right mt-1 mr-5 mb-2">Inscrever-se</button>
-            </div>
+        <div id="div-pag" class="h-auto w-[43rem] mt-10 relative block">
+        <div id="div-topo" class="w-full h-28">
+          <div id="div-header">
+            <p class="text-2xl mb-2">${title}</p>
+            <p class="font-semibold">Ong responsavel: ${nameOng}</p>
+            <p><em>Expira em: ${expira}</em></p>
+          </div>
+        </div>
+        <hr id="barra" class="w-full h-3">
+
+        <div class="w-full mb-10">
+          <p class="mx-5">
+            ${desc}
+          </p>
+          <div id="div-baixo" class="w-full h-10">
+            <button id="btn-subscribe" class="px-3 py-1 rounded-2xl float-right mt-1 mr-5 mb-2">Inscrever-se</button>
+          </div>
+        </div>
+    </div>
         
        `;
         $(`.divMeio`).append($(pagina));
@@ -43,6 +53,7 @@ function carregaPagina(){
 
 function request(method, url, headers, successCallback) {
     $.ajax({
+        
         type: method,
         url: `https://localhost:7070/${url}`,
         headers: headers,
