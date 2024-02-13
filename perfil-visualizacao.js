53f271fdaef6abdfb5b5283394ebe40195c2ac25
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-02-13 19:35:46 -0300

    Feito

--- perfil-visualizacao.js
+++ perfil-visualizacao.js
@@ -1,7 +1,7 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 //const id = urlParams.get('id');
-const id = 7;
+const id = 8;
 
 $(() => {
     //Faz requisição para o banco do tipo GET para pegar o usuario com o id especificado
@@ -106,59 +106,4 @@ function loadProfileImage(){
       contentType: "application/json",
       datatype: "json",
     });
-  }
-
-  function openConquestModal(achieId){
-    $.ajax({
-      type: "GET",
-      url: `https://localhost:7070/AchievementsUser/ConquistaUser/${id}/${achieId}`,
-      success: function(data){
-        loadAchievementInfo(achieId);
-        refactorDate(data.completionDate);
-        
-        $(`#modal-achievement`).addClass("modal-active");
-      },
-      header: {},
-      contentType: "application/json",
-      dataType: "json",
-    });
-  }
-
-  function refactorDate(date){
-    let correctDate = new Date(date);
-    date =  correctDate.toLocaleDateString();    
-    let achievementDate = document.getElementById('achievement-date-conclusion').innerHTML = "Alcançada em: " + date;
-  }
-  
-  function loadAchievementInfo(achieId){
-    $.ajax({
-      type: "GET",
-      url: `https://localhost:7070/Achievements/${achieId}`,
-      success: function(data){
-        let achievementImage = document.getElementById('achievement-icon-conclusion');
-        achievementImage.setAttribute('src', `${data.image}`);
-        let achievementDescription = document.getElementById('achievement-description').innerHTML = data.description;
-      },
-      header: {},
-      contentType: "application/json",
-      dataType: "json",
-    });
-  }
-
-  function removeItem(i) {
-    itemsList.splice(i, 1);
-    loadItems();
-  }
-  
-  const openModals = document.getElementsByClassName('open-modal');
-  const modalArray = Array.from(openModals).entries();
-  const modals = document.getElementsByClassName('modal');
-  const closeButtons = document.getElementsByClassName('close-modal');
-  
-  for (let [index, trigger] of modalArray) {
-    const toggleModal = () => {
-      modals[index].classList.toggle('modal-active');
-    }
-    trigger.addEventListener("click", toggleModal);
-    closeButtons[index].addEventListener("click", toggleModal);
   }
