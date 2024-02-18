95e7f2e59357a27d9d32c31909750e581dcda86b
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-02-18 16:34:30 -0300

    Alteração javascript

--- ongs-listajs.js
+++ ongs-listajs.js
@@ -12,8 +12,22 @@ window.onload = function() {
     try{
         const user = JSON.parse(localStorage.getItem("user"));
         const id = user.id;
-
+        const isNgo = localStorage.getItem("ong");
+        
+        
         if(id != null){
+            
+            if(isNgo == 'true'){
+                let link = document.getElementById('profile-page');
+                link.setAttribute("href","perfil-edicao-ong.html");
+                let linkSmall = document.getElementById('small-profile-page');
+                linkSmall.setAttribute("href","perfil-edicao-ong.html");
+            }else{            
+                let link = document.getElementById('profile-page');
+                link.setAttribute("href","perfil-edicao.html");
+                let linkSmall = document.getElementById('small-profile-page');
+                linkSmall.setAttribute("href","perfil-edicao.html");
+            }
             document.getElementById('btn-login').style.display = 'none';
             document.getElementById('btn-logout').style.display = 'block';
 
@@ -21,7 +35,6 @@ window.onload = function() {
             document.getElementById('btn-logout-small').style.display = 'block';
 
             document.getElementById('sign-up').style.display = 'none';
-            document.getElementById('btn-sign-up-small').style.display = 'none';
             document.getElementById('profile-page').style.display = 'block';
             document.getElementById('profile-page-small').style.display = 'block';
         }
