42c121071d966bfdad4032d78a5584dd22ed637d
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-02-18 16:05:21 -0300

    Header alterado em perfil de voluntario

--- perfil-edicao.js
+++ perfil-edicao.js
@@ -95,7 +95,28 @@ $(() => {
       dataType: "json",
     });
 
+    try{
+      const user = JSON.parse(localStorage.getItem("user"));
+      const id = user.id;
+
+      if(id != null){
+          document.getElementById('btn-login').style.display = 'none';
+          document.getElementById('btn-logout').style.display = 'block';
+
+          document.getElementById('btn-login-small').style.display = 'none';
+          document.getElementById('btn-logout-small').style.display = 'block';
+
+          document.getElementById('sign-up').style.display = 'none';
+          document.getElementById('btn-sign-up-small').style.display = 'none';
+          document.getElementById('profile-page').style.display = 'block';
+          document.getElementById('profile-page-small').style.display = 'block';
+      }
+  }catch{}
 })
+
+function logout(){
+  localStorage.clear();
+}
 
 function shareConquest(){
   const userAchie = {
