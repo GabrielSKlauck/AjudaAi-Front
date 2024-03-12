b6ac38998019713839a5b9544e561d65a76a552f
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-03-12 20:44:06 -0300

    Listagem de ong com sua causa feito

--- ongs-listajs.js
+++ ongs-listajs.js
@@ -49,17 +49,36 @@ function mostraOng(item) {
             <div id="${linha.id}" class="cards-ong md:mt-10 mt:ml-6 w-[20rem] h-20 rounded-3xl ml-10 mb-3" onclick="abrirPagina(${linha.id})">
             <h1 class="text-center mt-2">${linha.ngoName}</h1> <!-- Jomhura-->
             <div class="w-full flex justify-center mt-2 div-card-cause">
-              <h1 class="text-center card-cause">${getCauseName(linha.causesId)}</h1>
+              <h1 class="text-center card-cause w-auto" id="causa${linha.id}"></h1>
             </div>
             </div>
         `;
+        getCauseName(linha.causesId).then(function(causeName) {
+        document.getElementById(`causa${linha.id}`).innerText = causeName;
+        });
         $(`#container-ongs`).append($(cardOng));
         
     });
 }
 
 function getCauseName(id){
-
+    
+    return new Promise(function(resolve, reject) {
+        $.ajax({
+            type: "GET",
+            url: `https://localhost:7070/Causes/GetById/${id}`,
+            success: function(data) {
+                resolve(data.name);
+            },
+            error: function(xhr, status, error) {
+                reject(error); // Caso ocorra algum erro na requisição AJAX
+            },
+            header: {},
+            contentType: "application/json",
+            dataType: "json", // Correção de 'datatype' para 'dataType'
+        });
+    });
+    
 }
 
 function abrirPagina(id){
